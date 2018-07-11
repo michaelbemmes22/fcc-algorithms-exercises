121f8404d3e283ad7e0f623c039944aae4bf6a59
michaelbemmes22 <40074290+michaelbemmes22@users.noreply.github.com>
2018-07-11 11:02:32 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -25,11 +25,11 @@ $(document).ready(function() {
 function getData() {
     $.ajax ({
         type: "GET",
-        url: "https://api.mysportsfeeds.com/v1.2/pull/nba/2017-2018-regular/standings.json",
+        url: "https://api.mysportsfeeds.com/v2.0/pull/nba/2017-2018-regular/standings.json",
         dataType: 'json',
         async: false,
         headers: {
-            "Authorization": "Basic " + btoa("kmtate" + ":" + "mkjointrepo2832")
+            "Authorization": "Basic " + btoa("kmtate" + ":" + "MYSPORTSFEEDS")
             // "Authorization": "Basic" + "b604851b-0f22-4982-a034-675c03" + ":" + "mkjointrepo2832",
         },
         data: '{ "comment" }',
