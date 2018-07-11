e8bdf2b6b8b06cd5f8692c307a6e186bb03e21aa
Kevin Tate <2kmtate@gmail.com>
2018-07-11 17:44:02 -0400

    League standings

--- js/main.js
+++ js/main.js
@@ -1,39 +1,51 @@
 
 // I believe we can use this site for our API:     https://www.mysportsfeeds.com/data-feeds/
 
+// Just throwing some methods in here but we can architect and modularize later
+// Also decide how we serve up our HTML - i dont know enough about implmenting JSP or templates 
 
-
+var object = callApi().overallteamstandings.teamstandingsentry;
 
 
 $(document).ready(function() {
-    console.log("hi Bemmes :)");
 
+    object.forEach((el) => renderTeamRow(el));
 
-    var example = "<span>Hey Bemmes! This is how you can use jQuery to select DOM elements if you've never seen it -- \
-        super easy JS library to implement</span>";
-    $('#example').append(example);
+})
 
-    // Still trying to figure out the authentication for the ajax call which is currently not working and throwing some bad js errors
-    getData();
+function renderTeamRow(team) {
+    var rank = team.rank;
+    var teamCity = team.team.City;
+    var teamName = team.team.Name;
+    var gamesBack = team.stats.GamesBack["#text"];
+    var wins = team.stats.Wins["#text"];
+    var losses = team.stats.Losses["#text"];
 
-});
+    var standingsRow = 
+        "<div class=\"row\"> \
+            <div class=\"col-lg-1\">" +rank+ "</div> \
+            <div class=\"col-lg-2\">" +teamCity+ "</div> \
+            <div class=\"col-lg-2\">" +teamName+ "</div> \
+            <div class=\"col-lg-1\">" +gamesBack+ "</div> \
+            <div class=\"col-lg-1\">" +wins+ "</div> \
+            <div class=\"col-lg-1\">" +losses+ "</div> \
+        </div>";
 
+    $('.standings-rows').append(standingsRow);
+}
 
-
-
-
-function getData() {
-    $.ajax ({
+function callApi() {
+    return $.ajax ({
         type: "GET",
-        url: "https://api.mysportsfeeds.com/v1.2/pull/nba/2018-playoff/overall_team_standings.json",
+        url: "https://api.mysportsfeeds.com/v1.2/pull/nba/2017-2018-regular/overall_team_standings.json",
         dataType: 'json',
         async: false,
         headers: {
             "Authorization": "Basic " + btoa("b604851b-0f22-4982-a034-675c03" + ":" + "mkjointrepo2832")
         },
-        //data: '{ "comment" }',
+        // data: '{ "comment" }',
         success: function (data){
-            console.log(data);
+            return data;
         }
-    });
-};
+    }).responseJSON;
+}
